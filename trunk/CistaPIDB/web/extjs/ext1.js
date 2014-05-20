f752b91900b505f44a7c29c25181a79d3a87e541
tunghank@gmail.com <tunghank@gmail.com@c46e3d9e-1036-1963-d1c1-75e75108fa21>
2014-05-20 08:38:40 +0000

    ExtJS , Ex2

--- trunk/CistaPIDB/web/extjs/ext1.js
+++ trunk/CistaPIDB/web/extjs/ext1.js
@@ -2,8 +2,45 @@
 
 (function() {
 	Ext.onReady(function(){
-
-		Ext.MessageBox.alert("Hello", "Hello World");
+		//alert(0);
+		//Ext.MessageBox.alert("Hello", "Hello World");
+		var myArray = [1,2,3,4,-3,-4];
+		Ext.Array.every(myArray, function(item){
+			if(item>0){
+				return true;
+			}else{
+				//<=0
+				//alert(item);
+				return false;
+			}
+		
+		},this);
+		
+		var newArray = Ext.Array.filter(myArray,function(item){
+			if(item>0){
+				return true;
+			}else{
+				return false;
+			}
+		
+		},this);
+		//alert(newArray.join("\n"));
+		Object.prototype.get = function(key, defaultValue){
+			if(this[key]){
+				return this[key];
+			}else{
+				if(defaultValue){
+					return defaultValue;
+				}
+			}
+		}
+		var person = {
+			name:"Hank",
+			age:28
+		}
+		//alert(person['name']);
+		alert(person.get('name'));
+		
 	});
 
 })();
